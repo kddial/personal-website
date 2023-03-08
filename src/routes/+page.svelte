b8b245bec73bdd1968b387a15b130e052dab80e5
Kevin Dial <kevin.dial.phone@gmail.com>
2023-03-08 05:41:55 -0500

    copy view count file from old website

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<!-- github -->
+<!-- linkedIn -->
+
+<!-- toy projects -->
+<!-- Wavy game (html5 canvas) - Feb 2016 -->
+
+<!-- 1012 page views -->
