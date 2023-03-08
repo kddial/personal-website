25a28f2ff8eff5c031125a1bbeecafe0fa7e6cbf
Kevin Dial <kevin.dial.phone@gmail.com>
2023-03-08 06:44:38 -0500

    load icons from font awesome

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,42 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Hi! I'm Kevin, welcome to my site</h1>
 
-<!-- github -->
-<!-- linkedIn -->
+<!-- links -->
+<section class="links">
+  <a href="https://github.com/kddial" target="_blank" rel="noreferrer"
+    ><i class="fa fa-github" />github
+  </a>
+  <a href="https://ca.linkedin.com/in/kevindial" target="_blank" rel="noreferrer"
+    ><i class="fa fa-linkedin-square" />linkedIn
+  </a>
+  <a href="http://myanimelist.net/animelist/kddial?status=2" target="_blank" rel="noreferrer"
+    >myanimelist
+  </a>
+</section>
+
+<!-- work exp -->
+<section />
+
+<!-- school -->
+<section />
 
 <!-- toy projects -->
-<!-- Wavy game (html5 canvas) - Feb 2016 -->
+<section>
+  <a href="http://kevindial.com/wavy-game/" target="_blank" rel="noreferrer"
+    >Wavy game (html5 canvas) - Feb 2016</a
+  >
+</section>
+
+<!-- Get page views -->
 
 <!-- 1012 page views -->
+<style>
+  .links {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+  }
+
+  section {
+    margin: 2rem 0;
+  }
+</style>
