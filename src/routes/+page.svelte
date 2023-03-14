7639c49690fbbeb37dcc29dc52669c428a9eba1c
Kevin Dial <kevin.dial.phone@gmail.com>
2023-03-14 15:48:53 -0400

    add resume to homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,17 @@
-<h1>Hi! I'm Kevin, welcome to my site</h1>
+<script>
+  import ResumePreviewImg from '../static/Kevin_Dial_Resume_2023_v2_png.png';
+  import ResumePdf from '../static/Kevin_Dial_Resume_2023_v2.pdf';
+</script>
 
-<!-- links -->
-<section class="links">
-  <a href="https://github.com/kddial" target="_blank" rel="noreferrer"
-    ><i class="fa fa-github" />github
-  </a>
-  <a href="https://ca.linkedin.com/in/kevindial" target="_blank" rel="noreferrer"
-    ><i class="fa fa-linkedin-square" />linkedIn
-  </a>
-  <a href="http://myanimelist.net/animelist/kddial?status=2" target="_blank" rel="noreferrer"
-    >myanimelist
-  </a>
+<h1>Hi! I'm Kevin, welcome to my site :)</h1>
+
+<section>
+  <div class="work-container">
+    <a href={ResumePdf} target="_blank" rel="noreferrer">
+      <img class="work-image" src={ResumePreviewImg} alt="resume preview" height="100px" />
+    </a>
+    <div class="work-description"><strong>Resume</strong> Here is my resume updated for 2023</div>
+  </div>
 </section>
 
 <!-- work exp -->
@@ -39,7 +40,21 @@
   </div>
 </section>
 
+<!-- links -->
+<section class="links">
+  <a href="https://github.com/kddial" target="_blank" rel="noreferrer"
+    ><i class="fa fa-github" />github
+  </a>
+  <a href="https://ca.linkedin.com/in/kevindial" target="_blank" rel="noreferrer"
+    ><i class="fa fa-linkedin-square" />linkedIn
+  </a>
+  <a href="http://myanimelist.net/animelist/kddial?status=2" target="_blank" rel="noreferrer"
+    >myanimelist
+  </a>
+</section>
+
 <!-- Get page views -->
+<!-- todo use view-count.js -->
 
 <!-- 1012 page views -->
 <style>
@@ -48,5 +63,23 @@
     flex-direction: column;
     gap: 10px;
     margin: 2rem 0;
+  }
+
+  .work-container {
+    display: flex;
+  }
+  .work-image {
+    border: 3px solid black;
+    border-radius: 4px;
+    transition: 0.2s border-color;
+  }
+  .work-image:hover {
+    border-color: rgb(32, 47, 222);
+  }
+  .work-description {
+    padding-left: 1rem;
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
   }
 </style>
