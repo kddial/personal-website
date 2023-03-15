caffa2322699d3e56b6b3c728863929b3fc3755b
Kevin Dial <kevin.dial.phone@gmail.com>
2023-03-15 13:18:54 -0400

    add react component usage

--- src/routes/blog/the-space-component/+page.svelte
+++ src/routes/blog/the-space-component/+page.svelte
@@ -13,6 +13,38 @@
 </p>
 
 <div>
+  <h4>Usage</h4>
+  <code>
+    {`
+    <h1>Welcome to my internet corner!</h1>
+    <Space height={1} />
+    <p>This is where i share thoughts</p>
+    `}
+  </code>
+
+  <h4>React component</h4>
+  <code>
+    {`
+    // File name space.js
+
+    function Space(props) {
+      const { height = null, width = null } = props;
+
+      const style = {};
+      if (height) {
+        style.height = \`\${height}rem\`;
+      }
+
+      if (width) {
+        style.width = \`\${width}rem\`;
+      }
+
+      return <div className="space" style={style} />;
+    }
+    export default Space;
+    `}
+  </code>
+
   <h4>Svelte component</h4>
   <code>
     {`
@@ -32,15 +64,6 @@
     </script>
 
     <div class="space" style:width style:height />
-    `}
-  </code>
-
-  <h4>Svelte usage</h4>
-  <code>
-    {`
-    <h1>Welcome to my internet corner!</h1>
-    <Space height={1} />
-    <p>This is where i share thoughts that keep recurring</p>
     `}
   </code>
 </div>
