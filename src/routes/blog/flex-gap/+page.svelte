7f53f1b3f27e4bc1809342b595b9065518635c4d
Kevin Dial <kevin.dial.phone@gmail.com>
2023-03-15 13:57:16 -0400

    initial content for flex gap

---
+++ src/routes/blog/flex-gap/+page.svelte
@@ -0,0 +1,26 @@
+<h1>Using Flexbox Gap for uniform spacing</h1>
+
+<p>
+  Using gap inside flexbox is a simple solution to give you uniform
+  spacing between elements. Previously I would use margins on all
+  elements except for first and last child, which was a clunky
+  solution. The gap property was originally introduced for CSS Grids
+  but it has been brought over for flexbox support since 2021 (see
+  sources).
+</p>
+
+<a
+  target="_blank"
+  rel="noreferrer"
+  href="https://caniuse.com/flexbox-gap"
+>
+  CanIUse Flexbox gap
+</a>
+
+<a
+  target="_blank"
+  rel="noreferrer"
+  href="https://developer.mozilla.org/en-US/docs/Web/CSS/gap"
+>
+  MDN Browser compatibility for Gap in Flex layout
+</a>
